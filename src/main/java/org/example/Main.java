a608aaefdb789915aaec3bd01d740e018732c9f4
asrytikov <asrytikov@gmail.com>
2023-11-29 21:14:10 +0400

    Lesson28

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        People p = context.getBean(People.class);
+        System.out.println(p);
+        System.out.println(p.getName());
+
+        People x = new People();
+        x.setName("Masha");
+
+        Supplier<People> peopleSupplier = () -> x;
+        context.registerBean("people1", People.class, peopleSupplier, bc -> bc.setPrimary(true));
+
+        People x1 = context.getBean(People.class);
+        System.out.println(x1.getName());
     }
 }
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,42 +1,11 @@
 package org.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
 public class ProjectConfig {
-
-    @Bean(name = "alex")
-    People people(){
-        var p = new People();
-        p.setName("Alex");
-        return p;
-    }
-
-    @Primary
-    @Bean(value = "masha")
-    People people2(){
-        var p = new People();
-        p.setName("Masha");
-        return p;
-    }
-
-    @Bean("andrey")
-    People people3(){
-        var p = new People();
-        p.setName("Andrey");
-        return p;
-    }
-
-
-    @Bean
-    String hello(){
-        return "Hello";
-    }
-
-    @Bean
-    Integer ten(){
-        return 10;
-    }
 }
